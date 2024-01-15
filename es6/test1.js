cff2ef615c523d1c2811a0776b7604b1a604f739
dlxotjds2 <dlxotjds2@naver.com>
2024-01-15 09:49:25 +0900

    first_commit

---
+++ es6/test1.js
@@ -0,0 +1,4 @@
+function method1(){
+    console.log("method1");
+}
+export {method1};
---
+++ es6/test2.js
@@ -0,0 +1,11 @@
+function method2(){
+    console.log("method2");
+}
+
+
+//export 안함
+function method3(){
+    console.log("method3");
+}
+
+export default method2;

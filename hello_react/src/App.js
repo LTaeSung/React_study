b0bb99a8614be07607d10d7aafcb3f5feded1fdd
dlxotjds2 <dlxotjds2@naver.com>
2024-01-29 21:40:57 +0900

    hellow_react

---
+++ hello_react/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import './index.css';
+
+function App() {
+  return <h1>하이</h1>;
+}
+
+export default App;
---
+++ hello_react/src/Test.js
@@ -0,0 +1,6 @@
+const Test = ()=>{
+    return (
+    <div>멍청이 ㅎㅎ <div> 아니아니야</div>
+    </div>);
+}
+export default Test;
